437a8c82caf3a5ef50006d82c4bc7a01726a6ed6
Mira-23 <stanimira23@abv.bg>
2025-12-28 01:22:56 +0200

    rough logic and datasets

--- Wordle.py
+++ Wordle.py
@@ -1,29 +1,80 @@
-# import pygame
+#import pygame
+import random
+from typing import List
 
-random_word = "batch"
-letters = list(random_word)
+def startup() -> List[str]:
+    with open('word lists/fiveletterwords.txt', 'r') as file:
+        five_letter_words = file.read().splitlines()
+    with open('word lists/sixletterwords.txt', 'r') as file:
+        six_letter_words = file.read().splitlines()
+    with open('word lists/sevenletterwords.txt', 'r') as file:
+        seven_letter_words = file.read().splitlines()
 
-count = 0
+    mode = 5 # int(input())
 
-def is_word_valid(word: str) -> bool:
-    if word:
-        return True
-    else:
-        return False
+    chosen_list = []
+    match mode:
+        case 5:
+            chosen_list = five_letter_words
+        case 6:
+            chosen_list = six_letter_words
+        case 7:
+            chosen_list = seven_letter_words
+        case _:
+            pass
+    
+    return chosen_list
 
-while count!=6:
+def guessing(chosen_list: List[str]):
+    mode = len(chosen_list[0])
+    #number_of_guesses = manual choice (input) - (2,mode+1)
+    random_word = "ABOTB" # chosen_list[random.randint(0, len(chosen_list)-1)]
+    letters = list(random_word)
+    count = 0
 
-    guess = input()
-    if not is_word_valid(guess):
-        continue
-
-    count+=1
-    pass
-
+    def is_word_valid(word: str) -> bool:
+        if word:
+            return True
+        else:
+            return False
 
 
-print("damn, you lost!")
+    while count!=mode+1:
 
+        guess = (input()).upper()
+        guess_letters = list(guess)
+        if not is_word_valid(guess):
+            print("not a word my guy")
+            continue
+
+        #logic
+        if guess == random_word:
+            print("You won!")
+            return
+        else:
+            #make a list for alphabet - color corrects in green, guesses in yellow, wrongs in black
+            to_be_guessed = list(letters)
+
+            for index, letter in enumerate(letters):
+                if letter == guess_letters[index]:
+                    to_be_guessed.remove(guess_letters[index])
+                    print("g", end="")
+                    
+                elif guess_letters[index] in to_be_guessed:
+                    to_be_guessed.remove(guess_letters[index])
+                    print("y", end="")
+                    
+                else:
+                    print("b", end="")
+            print("\n")
+        count+=1
+        pass
+
+    print(f"damn, you lost! the word was {random_word}")
+
+chosen_list = startup()
+
+guessing(chosen_list)
 # pygame.init()
 
 # screen = pygame.display.set_mode((640,640))
