25fd695a524be60b5877ecf14f086651056298db
Mira-23 <stanimira23@abv.bg>
2026-01-05 00:15:17 +0200

    general improvements to core

--- Wordle.py
+++ Wordle.py
@@ -1,7 +1,8 @@
-from enum import Enum
 import pygame
 import random
+from enum import Enum
 from typing import List
+from typing import Optional
 
 def mode_choice(mode : int) -> tuple[List[str],List[str]]:
     with open('word lists/fiveletterwords.txt', 'r') as file:
@@ -43,49 +44,58 @@ def is_word_valid(word: str, chosen_list : List[str]) -> bool:
     else:
         return False
 
-def random_word(chosen_list: List[str]) -> List[str]:
+def choose_random_word(chosen_list: List[str]) -> List[str]:
     random_word = chosen_list[random.randint(0, len(chosen_list)-1)]
     letters = list(random_word)
     return letters
 
-#fix board type hint
+# fix board type hint
 def guessing(board, row: int,guess_letters: List[str], word : str) -> bool:
     guess = ''.join(guess_letters).upper()
     guess_letters = list(guess)
     letters = list(word.upper())
 
-    #logic
+    # logic
     if guess == word:
         for cell in board[row]:
             cell.color = CellColors.GREEN
         return True
     else:
-        #make a list for alphabet - color corrects in green, guesses in yellow, wrongs in black
+        # make a list for alphabet - color corrects in green, guesses in yellow, wrongs in black
         to_be_guessed = list(letters)
 
         for index, letter in enumerate(letters):
             if letter == guess_letters[index]:
+                if guess_letters[index] not in to_be_guessed:
+                    [setattr(cell, 'color', CellColors.BLACK) for cell in board[row] if cell.text == guess_letters[index] and cell.color == CellColors.YELLOW]
+                    to_be_guessed.append(guess_letters[index])
                 to_be_guessed.remove(guess_letters[index])
                 board[row][index].color = CellColors.GREEN
+                board[row][index].text = guess_letters[index]
                     
             elif guess_letters[index] in to_be_guessed:
                 to_be_guessed.remove(guess_letters[index])
                 board[row][index].color = CellColors.YELLOW
+                board[row][index].text = guess_letters[index]
                     
             else:
                 board[row][index].color = CellColors.BLACK
+                board[row][index].text = guess_letters[index]
     return False
 
 class CellColors(Enum):
-    BLACK = (0, 0, 0)
+    BLACK = (10, 10, 10)
     WHITE = (128, 128, 128)
-    YELLOW = (255, 255, 0)
+    YELLOW = (200, 200, 0)
     GREEN = (0, 255, 0)
 
 class Cell:
-    def __init__(self,letter='',color=CellColors.WHITE):
+    def __init__(self, letter: Optional[pygame.Surface] = None, color=CellColors.WHITE,x=0,y=0, text=''):
         self.letter = letter
         self.color = color
+        self.x = x
+        self.y = y
+        self.text = text
 
 pygame.init()
 
@@ -96,72 +106,87 @@ screen = pygame.display.set_mode((640,640))
 clock = pygame.time.Clock()
 delta_time = 0.1
 
-#this needs to be changed during runtime
+# this needs to be changed during runtime
 mode = 5 #5,6,7
 amount_of_guesses = number_of_guesses()
 chosen_list, longer_list = mode_choice(mode)
-chosen_word = random_word(chosen_list)
+chosen_word = choose_random_word(chosen_list)
 
-#change list length depending on number of guesses
-guess_list = [[],[],[],[],[],[]]
+guess_list = [[] for _ in range(amount_of_guesses)]
 word_number = 0
+letter_number = 0
 
 valid_letter_inputs = [getattr(pygame, f"K_{chr(i)}") for i in range(ord('a'), ord('z') + 1)]
 
-round_active = True
-
 grid_size, grid_height, cell_size = mode, amount_of_guesses, 50
 board = [[Cell() for _ in range(grid_size)] for _ in range(grid_height)]
+
+round_active = True
 
 running = True
 
 pygame.display.set_caption('PVP Wordle')
 while running:
-    #closing the window
+    # closing the window
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
         if round_active:
             if event.type == pygame.KEYDOWN:
+                # delete character
                 if event.key == pygame.K_BACKSPACE:
-                    #delete character
+
                     guess_list[word_number] = guess_list[word_number][:-1]
-                elif event.key == pygame.K_RETURN and len(guess_list[word_number]) == 5:
+
+                    if letter_number != 0:
+                        letter_number-=1
+
+                    board[word_number][letter_number].letter = my_font.render(''.upper(),False,(250,250,250))
+
+                elif event.key == pygame.K_RETURN and len(guess_list[word_number]) == mode:
                     if not is_word_valid(''.join(guess_list[word_number]),longer_list):
-                        guess_list[word_number] = []
+                        print("Invalid word")
                         continue
 
                     if guessing(board,word_number,guess_list[word_number],''.join(chosen_word)):
                         round_active = False
                         print("You Won!")
+                        
                     else:
                         word_number+=1
 
                         if word_number == amount_of_guesses:
                             round_active = False
-                            print("You Lost!")
+                            print(f"You Lost! The word was {''.join(chosen_word)}")
+                            
+                    letter_number = 0
 
-                elif event.key in valid_letter_inputs and len(guess_list[word_number])<5:
+                elif event.key in valid_letter_inputs and len(guess_list[word_number])<mode:
                     guess_list[word_number].append(event.unicode)
+                    board[word_number][letter_number].letter = my_font.render(str(event.unicode).upper(),False,(250,250,250))
+                    letter_number+=1
 
-    #white background
+    # white background
     screen.fill((255,255,255))
 
-
+    # display each cell
     for iy, rowOfCells in enumerate(board):
         for ix, cell in enumerate(rowOfCells):
             cell_rect = pygame.Rect(ix*cell_size+180, iy*cell_size+100, cell_size-5, cell_size-5)
-            c_sur = my_font.render(cell.letter,False,(250,250,250))
+            cell.x = ix*cell_size+180 + 15
+            cell.y = iy*cell_size+100 + 3
             pygame.draw.rect(screen, cell.color.value, cell_rect)
  
-    text_surface = my_font.render("\n".join(" ".join(str(letter) for letter in word) for word in guess_list), False, (250, 250, 250))
+    # display letter per cell
+    for row in board:
+        for cell in row:
+            if cell.letter is not None:
+                screen.blit(cell.letter, (cell.x,cell.y))
 
-    screen.blit(text_surface, (250,100))
-
-    #display stuff on the window
+    # display stuff on the window
     pygame.display.flip()
 
-    #time handling for framerate
+    # time handling for framerate
     delta_time = clock.tick(60) / 1000
     delta_time = max(0.001, min(0.1,delta_time))
 
